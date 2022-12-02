819ee80fbf428771ad23bf4ccc61234c712b4e6f
dejohansson <33880373+dejohansson@users.noreply.github.com>
2022-12-02 21:07:50 +0100

    Day 2

--- Day2/Program.fs
+++ Day2/Program.fs
@@ -6,9 +6,35 @@ let main argv =
     let lines = File.ReadAllLines filename
     printfn $"Read {lines.Length} lines from {filename}\n"
 
-    printfn $"Part 1:"
+    let score1 =
+        lines
+        |> Array.map (fun l ->
+            let plays = ("ABC".IndexOf(l[0]) + 1, "XYZ".IndexOf(l[2]) + 1)
 
-    //printfn ""
-    //printfn $"Part 2:"
+            snd plays
+            + match plays with
+              | o, y when (o = 3 && y = 1) -> 6
+              | o, y when (o = 1 && y = 3) -> 0
+              | o, y when o < y -> 6
+              | o, y when o = y -> 3
+              | _ -> 0)
+        |> Array.sum
 
+    printfn $"Part 1: {score1}"
+
+    let score2 =
+        lines
+        |> Array.map (fun l ->
+            let plays = ("ABC".IndexOf(l[0]), "XYZ".IndexOf(l[2]) * 3)
+
+            snd plays
+            + match plays with
+              | o, 0 -> (((o - 1) % 3) + 3) % 3
+              | o, 3 -> o
+              | o, 6 -> (o + 1) % 3
+              | _ -> raise <| new System.Exception("Opps :(")
+            + 1)
+        |> Array.sum
+
+    printfn $"Part 2: {score2}"
     0
