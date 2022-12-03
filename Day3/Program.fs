78f3e91d2ac6ed86c2291dd81ace92cd1a193fa4
dejohansson <33880373+dejohansson@users.noreply.github.com>
2022-12-03 14:24:31 +0100

    Day 3

--- Day3/Program.fs
+++ Day3/Program.fs
@@ -2,13 +2,39 @@
 
 [<EntryPoint>]
 let main argv =
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     let filename = Array.tryItem 0 argv |> Option.defaultValue "./input.txt"
-    let lines = File.ReadAllLines filename
-    printfn $"Read {lines.Length} lines from {filename}\n"
+    let rucksacks = File.ReadAllLines filename
+    printfn $"Read {rucksacks.Length} lines from {filename}\n"
+    let items = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    printfn $"Part 1:"
+    let sum =
+        rucksacks
+        |> Array.map (fun rucksack ->
+            let firstCompartment = rucksack |> Seq.take (rucksack.Length / 2) |> Set.ofSeq
 
-    //printfn ""
-    //printfn $"Part 2:"
+            let secondCompartment =
+                rucksack |> Seq.skip (rucksack.Length / 2) |> Set.ofSeq
 
+            Set.intersect firstCompartment secondCompartment |> Seq.head |> items.IndexOf)
+        |> Array.sum
+
+    printfn $"Part 1: {sum}"
+
+    let sum2 =
+        rucksacks
+        |> Seq.chunkBySize 3
+        |> Seq.fold
+            (fun sum group ->
+                sum
+                + (Set.intersectMany (
+                    group |> Array.map (fun rucksack -> Set.ofSeq rucksack)
+                   )
+                   |> Seq.head
+                   |> (items.IndexOf: char -> int)))
+            0
+
+    printfn $"Part 2: {sum2}"
+    stopWatch.Stop()
+    printfn $"Completed in {stopWatch.Elapsed.TotalMilliseconds} ms"
     0
