338735c30c218387c1fff4958f181759d7ad4859
dejohansson <33880373+dejohansson@users.noreply.github.com>
2022-12-04 21:55:08 +0100

    Day 4

--- Day4/Program.fs
+++ Day4/Program.fs
@@ -1,13 +1,36 @@
 ﻿open System.IO
+let add a b = fst a + fst b, snd a + snd b
 
 [<EntryPoint>]
 let main argv =
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     let filename = Array.tryItem 0 argv |> Option.defaultValue "./input.txt"
-    let lines = File.ReadAllLines filename
-    printfn $"Read {lines.Length} lines from {filename}\n"
-    printfn $"Part 1:"
-    printfn $"Part 2:"
+    let pairs = File.ReadAllLines filename
+    printfn $"Read {pairs.Length} lines from {filename}\n"
+
+    let res =
+        Seq.fold
+            (fun res (pair: string) ->
+                let s1, s2, s3, s4 =
+                    match
+                        pair.Split ','
+                        |> Array.map (fun elf -> elf.Split '-' |> Array.map int)
+                    with
+                    | [| [| s1; s2 |]; [| s3; s4 |] |] -> s1, s2, s3, s4
+                    | _ -> failwith ":("
+
+                (if (s1 <= s3 && s2 >= s4) || (s3 <= s1 && s4 >= s2) then
+                     (1, 1)
+                 else if (s1 <= s3 && s2 >= s3) || (s1 <= s4 && s2 >= s4) then
+                     (0, 1)
+                 else
+                     (0, 0))
+                |> add res)
+            (0, 0)
+            pairs
+
+    printfn $"Part 1: {fst res}"
+    printfn $"Part 2: {snd res}"
     stopWatch.Stop()
     printfn $"Completed in {stopWatch.Elapsed.TotalMilliseconds} ms"
     0
---
+++ Day5/Program.fs
@@ -0,0 +1,13 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let filename = Array.tryItem 0 argv |> Option.defaultValue "./input.txt"
+    let lines = File.ReadAllLines filename
+    printfn $"Read {lines.Length} lines from {filename}\n"
+    printfn $"Part 1:"
+    printfn $"Part 2:"
+    stopWatch.Stop()
+    printfn $"Completed in {stopWatch.Elapsed.TotalMilliseconds} ms"
+    0
