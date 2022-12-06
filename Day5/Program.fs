2dbb66711325136a562bb0c9202aadabe83e507b
dejohansson <33880373+dejohansson@users.noreply.github.com>
2022-12-06 19:36:30 +0100

    Day 6

--- Day5/Program.fs
+++ Day5/Program.fs
@@ -21,47 +21,36 @@ let main argv =
                 grid[*, column] |> Array.filter ((<>) ' ') |> List))
 
     let initState2 =
-        strState.Split("\r\n")[..^1]
-        |> Array.map (fun r ->
-            Seq.chunkBySize 4 r[1..] |> Seq.toArray |> Array.map (fun x -> x[0]))
-        |> array2D
-        |> (fun grid ->
-            Array.init (Array2D.length2 grid) (fun column ->
-                grid[*, column] |> Array.filter ((<>) ' ') |> List))
+        initState
+        |> Array.map (fun (x: List<char>) -> x |> Seq.map (fun (y: char) -> y) |> List)
 
-    let part1 =
+    let part1, part2 =
         Seq.fold
-            (fun (state: List<char>[]) (instr: string) ->
+            (fun ((state1: List<char>[]), (state2: List<char>[])) (instr: string) ->
                 let amount, src, dest =
                     match (instr.Split(' ')) with
                     | [| _; amount; _; src; _; dest |] ->
                         amount |> int, src |> int, dest |> int
                     | _ -> failwith ":("
 
-                let crates = state[ (src - 1) ].GetRange(0, amount)
+                let crates = state1[ (src - 1) ].GetRange(0, amount)
                 crates.Reverse()
-                state[ dest - 1 ].InsertRange(0, crates)
-                state[ src - 1 ].RemoveRange(0, amount)
-                state)
-            initState
+                state1[ dest - 1 ].InsertRange(0, crates)
+                state1[ src - 1 ].RemoveRange(0, amount)
+                state2[ dest - 1 ].InsertRange(0, state2[ (src - 1) ].GetRange(0, amount))
+                state2[ src - 1 ].RemoveRange(0, amount)
+                (state1, state2))
+            (initState, initState2)
             (strInstr.Split("\r\n"))
-        |> Seq.fold (fun (res: string) (stack: List<char>) -> res + string stack[0]) ""
-
-    let part2 =
-        Seq.fold
-            (fun (state: List<char>[]) (instr: string) ->
-                let amount, src, dest =
-                    match (instr.Split(' ')) with
-                    | [| _; amount; _; src; _; dest |] ->
-                        amount |> int, src |> int, dest |> int
-                    | _ -> failwith ":("
-
-                state[ dest - 1 ].InsertRange(0, state[ (src - 1) ].GetRange(0, amount))
-                state[ src - 1 ].RemoveRange(0, amount)
-                state)
-            initState2
-            (strInstr.Split("\r\n"))
-        |> Seq.fold (fun (res: string) (stack: List<char>) -> res + string stack[0]) ""
+        |> (fun (s1, s2) ->
+            (s1
+             |> Seq.fold
+                 (fun (res: string) (stack: List<char>) -> res + string stack[0])
+                 ""),
+            s2
+            |> Seq.fold
+                (fun (res: string) (stack: List<char>) -> res + string stack[0])
+                "")
 
     printfn $"Part 1: {part1}"
     printfn $"Part 2: {part2}"
---
+++ Day6/Program.fs
@@ -0,0 +1,27 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let filename = Array.tryItem 0 argv |> Option.defaultValue "./input.txt"
+    let datastream = File.ReadAllText filename
+
+    let part1 =
+        (datastream
+         |> Seq.windowed 4
+         |> Seq.tryFindIndex (fun x -> x |> Seq.distinct |> Seq.length = 4))
+            .Value
+        + 4
+
+    let part2 =
+        (datastream
+         |> Seq.windowed 14
+         |> Seq.tryFindIndex (fun x -> x |> Seq.distinct |> Seq.length = 14))
+            .Value
+        + 14
+
+    printfn $"Part 1: {part1}"
+    printfn $"Part 2: {part2}"
+    stopWatch.Stop()
+    printfn $"Completed in {stopWatch.Elapsed.TotalMilliseconds} ms"
+    0
